2f728b758303e259dafb0a1109856c4e77338498
heffdev <email@heffdev.com>
2015-03-13 17:18:59 +0100

    added sabine as friend

--- UD2D/www/js/services.js
+++ UD2D/www/js/services.js
@@ -8,7 +8,7 @@ angular.module('starter.services', [])
 
   // Some fake testing data
   var friends = [
-    { id: 0, name: 'Scruff McGruff' },
+    { id: 0, name: 'Sabine Hefferan' },
     { id: 1, name: 'Brandon Hefferan' },
     { id: 2, name: 'Miss Frizzle' },
     { id: 3, name: 'Ash Ketchum' }
